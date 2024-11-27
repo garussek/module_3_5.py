838a31b66dd5c26f7df36bed42bf2d8750afe305
garussek <elena.gooss@gmail.com>
2024-11-27 20:36:16 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,17 @@
+def get_multiplied_digits(number: int):
+    str_number = str(number)
+    first = int(str_number[0])
+
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+    elif first != 0:
+        return first
+    else:
+        return 1
+
+
+result = get_multiplied_digits(40203)
+print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
